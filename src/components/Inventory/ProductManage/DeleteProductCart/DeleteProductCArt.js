4011d3244770d94c7a0fa90c44891cd36ff4876f
alamin-32 <alaminhussain32@gmail.com>
2022-05-01 02:57:18 +0600

    delete button added

--- src/components/Inventory/ProductManage/DeleteProductCart/DeleteProductCArt.js
+++ src/components/Inventory/ProductManage/DeleteProductCart/DeleteProductCArt.js
@@ -1,15 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './DeleteProductCart.css'
 
 const DeleteProductCArt = ({ product }) => {
     const { img, name } = product
+    const [products, setProducts] = useState({})
 
+    const handleProductsDelete = id => {
+        const proceed = window.confirm('Are you sure?')
+        // if (proceed) {
+
+        // }
+
+        const url = `http://localhost:5000/products/${id}`
+        fetch(url, {
+            method: 'DELETE',
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data)
+                if (data.deletedCount > 0) {
+                    console.log('deleted')
+                    const remaining = products.filter(products => products._id !== id)
+                    setProducts(remaining)
+                }
+
+            })
+
+
+    }
     return (
         <div>
             <div className='dlt-cart'>
                 <img className='dlt-img' src={img} alt="" />
                 <h3>{name}</h3>
-                <button className='btn btn-primary ms-3'>Remove</button>
+                <button onClick={() => handleProductsDelete(product._id)} className='btn btn-primary ms-3'>Remove</button>
             </div>
 
         </div>
